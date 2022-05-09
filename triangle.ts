fc75742e99af234753caa44fac404d35667de505
Julie <jatkin80@ford.com>
2022-05-09 17:28:04 -0500

    solve exercises

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,26 @@
-export class Triangle {}
+export class Triangle {
+  isTriangle: boolean;
+  a: number;
+  b: number;
+  c: number;
+
+  constructor(...sides: [number, number, number]) {
+    [this.a, this.b, this.c] = [...sides];
+    this.isTriangle =
+      this.a + this.b > this.c &&
+      this.a + this.c > this.b &&
+      this.b + this.c > this.a;
+  }
+  get isEquilateral(): boolean {
+    return this.isTriangle && this.a == this.b && this.a == this.c;
+  }
+  get isIsosceles(): boolean {
+    return (
+      this.isTriangle &&
+      (this.a == this.b || this.b == this.c || this.a == this.c)
+    );
+  }
+  get isScalene(): boolean {
+    return this.isTriangle && !this.isIsosceles;
+  }
+}
